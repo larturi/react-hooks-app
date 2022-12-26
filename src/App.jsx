6a1ba3c5da80261db48433813135862232d55ff1
Leandro Arturi <lea.arturi@gmail.com>
2022-12-26 12:35:28 -0300

    Initialize project using Create React App

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Header from './components/Header'
+
+const App = () => {
+  return (
+    <div className="App">
+      <Header />
+      <h1>Hola mundo</h1>
+    </div>
+  )
+}
+
+export default App
