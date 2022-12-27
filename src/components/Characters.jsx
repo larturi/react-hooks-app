c54e2d0b392df606c839cc89649734a09b657d73
Leandro Arturi <lea.arturi@gmail.com>
2022-12-27 13:59:27 -0300

    UseReducer

--- src/components/Characters.jsx
+++ src/components/Characters.jsx
@@ -1,7 +1,25 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useReducer } from 'react'
+
+const initialState = {
+  favorites: [],
+}
+
+const favoriteReducer = (state, action) => {
+  switch (action.type) {
+    case 'ADD_TO_FAVORITE':
+      return {
+        ...state,
+        favorites: [...state.favorites, action.payload],
+      }
+    default:
+      return state
+  }
+}
 
 const Characters = () => {
   const [characters, setCharacters] = useState([])
+
+  const [favorites, dispatch] = useReducer(favoriteReducer, initialState)
 
   useEffect(() => {
     fetchData()
@@ -13,10 +31,22 @@ const Characters = () => {
     setCharacters(data.results)
   }
 
+  const handleClick = (favorite) => {
+    dispatch({ type: 'ADD_TO_FAVORITE', payload: favorite })
+  }
+
   return (
     <div className="characters">
+      {favorites.favorites.map((favorite) => (
+        <li key={favorite.id}>{favorite.name}</li>
+      ))}
       {characters.map((character) => (
-        <h2 key={character.id}>{character.name}</h2>
+        <>
+          <h2 key={character.id}>{character.name}</h2>
+          <button type="button" onClick={() => handleClick(character)}>
+            Agregar a favoritos
+          </button>
+        </>
       ))}
     </div>
   )
